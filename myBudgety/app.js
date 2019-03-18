9b5dfc447baa0bf856009c6620be72b34ef8a68e
franciscobrunofilgueiras <fbfdsouza@gmail.com>
2019-03-18 19:58:34 -0300

    adding event handler to key pressed

--- myBudgety/app.js
+++ myBudgety/app.js
@@ -14,7 +14,7 @@ var budgetController = (function(){
         
         getExpenses : function(){
             return expenses.forEach(function(entry){
-                console.log(entry);
+                console.log(entry.value);
             });
         },
         
@@ -31,20 +31,32 @@ var UIController = (function(){
 })();
 
 var controller = (function(budgeCtrl, UICtrl){
-    document.querySelector('.add__btn').addEventListener('click', function(){
-       
-        var description = document.querySelector('.add__description').value;
-        var value = document.querySelector('.add__value').value;
-
-        budgeCtrl.addExpenses(description,value);
-            
-        console.log(budgeCtrl.getExpenses());
-        console.log(budgeCtrl.getIncome());
-        
     
-        document.querySelector('.add__description').value = '';
-        document.querySelector('.add__value').value = '';
-        
+    var ctrlAddItem = function() {
+        console.log('It works');
+    }
+    
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    
+    
+    //        var description = document.querySelector('.add__description').value;
+//        var value = document.querySelector('.add__value').value;
+//
+//        budgeCtrl.addExpenses(description,value);
+//            
+//        console.log(budgeCtrl.getExpenses());
+//        console.log(budgeCtrl.getIncome());
+//        
+//    
+//        document.querySelector('.add__description').value = '';
+//        document.querySelector('.add__value').value = '';
+    
+    
+    
+    document.addEventListener('keypress', function(event){
+        if(event.keyCode===13 || event.which ===13){
+            ctrlAddItem();
+        }
     });
    
 })(budgetController, UIController);
