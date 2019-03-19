5824b6222089b501c8731e3d5f1793d66dc8ea24
Bruno Filgueiras <fbfdsouza@gmail.com>
2019-03-19 19:28:15 -0300

    adding function constructors into the budget controller and storing them in one data object

--- myBudgety/app.js
+++ myBudgety/app.js
@@ -1,6 +1,26 @@
 var budgetController = (function () {
-
-
+    
+    var Expense = function(id, description, value){
+        this.id=id;
+        this.description=description;
+        this.value = value;
+    };
+    
+     var Income = function(id, description, value){
+        this.id=id;
+        this.description=description;
+        this.value = value;
+    };
+    
+    var data = {
+            allItems:{
+                inc: [],
+                exp:[]} 
+            },
+            totals:{
+                inc:0,
+                exp:0
+            }
 })();
 
 
@@ -50,14 +70,14 @@ var controller = (function (budgeCtrl, UICtrl) {
 
     var ctrlAddItem = function () {
         console.log(UICtrl.getInput());
-    }
+    };
     
     return {
         init: function(){
             console.log('Application has started');
             setupEventListeners();
         }
-    }
+    };
 
 
 
