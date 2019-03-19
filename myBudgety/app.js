e81ccf5da95a4c90aa7530918480537f1542faa0
Bruno Filgueiras <fbfdsouza@gmail.com>
2019-03-19 19:02:09 -0300

    added init function for starting up event listeners

--- myBudgety/app.js
+++ myBudgety/app.js
@@ -1,77 +1,76 @@
-var budgetController = (function(){
-    
-    var expenses = [];
-    var income = [];
-    
-    return {
-        addExpenses : function(description, value){
-            expenses.push({'description': description,'value': value});
-        },
-        
-        addIncome : function(description, value){
-            income.push({'description': description,'value': value});
-        },
-        
-        getExpenses : function(){
-            return expenses.forEach(function(entry){
-                console.log(entry.value);
-            });
-        },
-        
-        getIncome : function(){
-            return income;
-        }
-    }
-    
+var budgetController = (function () {
+
+
 })();
 
 
-var UIController = (function(){
-    
+var UIController = (function () {
+
     var DomStrings = {
-        inputType : '.add__type',
+        inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
         addBtn: '.add__btn'
-        
+
     };
-    
+
     return {
-        
-        getInput : function(){
-           return {
-            inputType: document.querySelector(DomStrings.inputType).value,
-            inputDescription: document.querySelector(DomStrings.inputDescription).value,
-            inputValue: document.querySelector(DomStrings.inputValue).value
-           };
-           
+
+        getInput: function () {
+            return {
+                inputType: document.querySelector(DomStrings.inputType).value,
+                inputDescription: document.querySelector(DomStrings.inputDescription).value,
+                inputValue: document.querySelector(DomStrings.inputValue).value
+            };
+
         },
-        
-        getDomStrings : function(){
+
+        getDomStrings: function () {
             return DomStrings;
         }
-        
+
     };
-    
+
 })();
 
-var controller = (function(budgeCtrl, UICtrl){
-    var DOM = UICtrl.getDomStrings();
-    
-    var ctrlAddItem = function() {
+var controller = (function (budgeCtrl, UICtrl) {
+
+    var setupEventListeners = function () {
+        var DOM = UICtrl.getDomStrings();
+
+        document.querySelector(DOM.addBtn).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+
+    };
+
+
+    var ctrlAddItem = function () {
         console.log(UICtrl.getInput());
     }
     
-    document.querySelector(DOM.addBtn).addEventListener('click', ctrlAddItem);
-    
-
-    
-    
-    
-    document.addEventListener('keypress', function(event){
-        if(event.keyCode===13 || event.which ===13){
-            ctrlAddItem();
+    return {
+        init: function(){
+            console.log('Application has started');
+            setupEventListeners();
         }
-    });
-   
+    }
+
+
+
 })(budgetController, UIController);
+
+controller.init();
+
+/*
+
+	1 - Get the field input data
+	2 - Add the item to the budget controller
+	3 - Add the item to the UI
+	4 - Calculate budget
+	5 - Display the budget on the UI
+
+*/
