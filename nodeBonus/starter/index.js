f6721903cdf529f93f613a4e56189ae32841acbf
franciscobrunofilgueiras <fbfdsouza@gmail.com>
2019-04-29 14:02:27 -0300

    adding laptop template

--- nodeBonus/starter/index.js
+++ nodeBonus/starter/index.js
@@ -4,7 +4,7 @@ const url = require("url");
 
 const file = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 
-const dataJson = JSON.parse(file);
+const laptops = JSON.parse(file);
 
 const server = http.createServer((request, response) => {
   const pathName = url.parse(request.url, true).pathname;
@@ -15,7 +15,25 @@ const server = http.createServer((request, response) => {
     response.end("products page");
   } else if ((pathName === "/laptop") & (id < 5)) {
     response.writeHead(200, { "Content-Type": "text/html" });
-    response.end(`laptop page for laptop ${id}`);
+
+    const laptop = laptops[id];
+    fs.readFile(
+      `${__dirname}/templates/template-laptop.html`,
+      "utf-8",
+      (err, data) => {
+        let output = data.replace(/{%NAME%}/g, laptop.productName);
+        output = output.replace(/{%IMAGE%}/g, laptop.image);
+        output = output.replace(/{%CPU%}/g, laptop.cpu);
+        output = output.replace(/{%RAM%}/g, laptop.ram);
+        output = output.replace(/{%STORAGE%}/g, laptop.storage);
+        output = output.replace(/{%PROCESSOR%}/g, laptop.cpu);
+        output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+        output = output.replace(/{%PRICE%}/g, laptop.price);
+        output = output.replace(/{%SCREEN%}/g, laptop.screen);
+
+        response.end(output);
+      }
+    );
   } else {
     response.writeHead(404, { "Content-Type": "text/html" });
     response.end("page doesn't exist");
